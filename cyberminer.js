b61832abde8788710516eee99e8961ac657b06b9
Anessia Mejia <Anessia.Santillana@gmail.com>
2023-06-20 19:31:51 -0500

    Button action
    added button action to display results next page

---
+++ cyberminer.js
@@ -0,0 +1,3 @@
+<><button id="search-btn" class="search-section">Home</button><script type="text/javascript">
+    document.getElementById("search-btn").onclick = function () {location.href = "results.html"};
+</script></>
