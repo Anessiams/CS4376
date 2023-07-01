1ead6bad300300c2955dd1426b5d8b7333fca8a1
Anessia Mejia <Anessia.Santillana@gmail.com>
2023-06-30 22:11:08 -0500

    Direct URL nav

--- cyberminer.js
+++ cyberminer.js
@@ -1,3 +1,4 @@
-<><button id="search-btn" class="search-section">Home</button><script type="text/javascript">
+<><button id="search-btn" class="search-section">Home</button>
+<script type="text/javascript">
     document.getElementById("search-btn").onclick = function () {location.href = "results.html"};
 </script></>
