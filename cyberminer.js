d2b14495cd992928c6c8231bf46c5aa82639cc4d
Anessia Mejia <Anessia.Santillana@gmail.com>
2023-07-19 21:02:22 -0500

    UI update
    UI Update to house project documents and added navbar functionality

--- cyberminer.js
+++
@@ -1,4 +0,0 @@
-<><button id="search-btn" class="search-section">Home</button>
-<script type="text/javascript">
-    document.getElementById("search-btn").onclick = function () {location.href = "results.html"};
-</script></>
